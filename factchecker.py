b004c47d1405a2b8dc84fd2819a17a8493bd40f2
Yinnii <y.yin94@outlook.de>
2023-06-25 11:31:33 +0200

    add multiple inputs

--- factchecker.py
+++ factchecker.py
@@ -1,38 +1,43 @@
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from langchain.chains import SimpleSequentialChain
+from langchain.chains import SimpleSequentialChain, SequentialChain
 import sys
 from dotenv import load_dotenv
 
-def fact_check(questionAnswer):
+def fact_check(question, answer):
     llm = OpenAI(temperature=0.3)
 
-    template = """Here is the question and the answer to the question. Make a bullet point list of the assumptions in the answer.\n\n"""
-    prompt_template = PromptTemplate(input_variables=["questionAnswer"], template="f{questionAnswer}\n\n template")
-    assumptions_chain = LLMChain(llm=llm, prompt=prompt_template)
+    dict = {"question": question, "answer": answer}
+    template = """Here is the question: {question} and the answer: {answer} to the question. Make a bullet point list of the assumptions in the answer.\n\n"""
+    prompt_template = PromptTemplate(input_variables=["question", "answer"], template=template)
+    assumptions_chain = LLMChain(llm=llm, prompt=prompt_template, output_key="assertions")
 
     template = """Here is a bullet point list of assertions:
     {assertions}
     For each assertion, determine whether it is true or false. If it is false, explain why.\n\n"""
     prompt_template = PromptTemplate(input_variables=["assertions"], template=template)
-    fact_checker_chain = LLMChain(llm=llm, prompt=prompt_template)
+    fact_checker_chain = LLMChain(llm=llm, prompt=prompt_template, output_key="facts")
 
-    template = """In light of the above facts, how would you answer the question.""".format(questionAnswer)
+    template = """In light of the above facts, how would you answer the question and explain your answer.""".format(question)
     template = """{facts}\n""" + template
     prompt_template = PromptTemplate(input_variables=["facts"], template=template)
-    answer_chain = LLMChain(llm=llm, prompt=prompt_template)
+    answer_chain = LLMChain(llm=llm, prompt=prompt_template, output_key="output")
 
-    overall_chain = SimpleSequentialChain(chains=[assumptions_chain, fact_checker_chain, answer_chain], verbose=True)
+    overall_chain = SequentialChain(chains=[assumptions_chain, fact_checker_chain, answer_chain],
+                                    input_variables=["question","answer"],
+                                    output_variables=["assertions", "facts", "output"],
+                                    verbose=True)
 
-    return overall_chain.run(questionAnswer)
+    return overall_chain(dict)
 
 if __name__=="__main__":
     load_dotenv()
     while True:
         print()
-        questionAnswer = input("Question and Answer: ")
-        answer = fact_check(questionAnswer)
-        print(answer)
+        question = input("Question: ")
+        answer = input("Answer: ")
+        response = fact_check(question,answer)
+        print(response)
     
     
