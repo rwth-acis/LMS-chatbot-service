16c06f7a60eec1db282ff6ed398a11f04bb2fc44
Yinnii <y.yin94@outlook.de>
2023-06-28 13:40:54 +0200

    add chat history

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 from langchain.schema import HumanMessage, AIMessage
+from langchain.memory import ConversationBufferMemory
 from prompts import split_prompt_question
-from pineconeDatabase import answer_retrieval, question_retrieval
+from indexRetriever import doc_retrieval, question_retrieval, question_answer, summarization
 from factchecker import fact_check
 from dotenv import load_dotenv
 
 if __name__=="__main__":
     load_dotenv()
     
-    chat_history = []
+    chat_history = ConversationBufferMemory()
     
     while True:
         print()
         questions = input("Question: ")
+        chat_history.chat_memory.add_user_message(questions)
         question_split = split_prompt_question(questions)
         subquestion = question_retrieval(question_split)
+        subquestion_answer = ""
         for i in range(len(subquestion)):
-            answer = answer_retrieval(subquestion[i])
-            chat_history.append(HumanMessage(content=subquestion[i]))
-            chat_history.append(AIMessage(content=answer))
-        print(chat_history)
-        
+            doc = doc_retrieval(subquestion[i])
+            answers = question_answer(subquestion[i], doc)
+            checks = fact_check(subquestion[i], answers)
+            subquestion_answer += checks["answer"] 
+            # summarize answers
+        overall_answer = summarization(subquestion_answer)
+        print(overall_answer)
+        chat_history.chat_memory.add_ai_message(overall_answer)
+        # history in mongodb abspeichern
