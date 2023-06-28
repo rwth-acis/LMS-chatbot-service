1e6bf28e2b63b68319115659ee70eedb5a9020b8
Yinnii <y.yin94@outlook.de>
2023-06-28 22:59:35 +0200

    add mongodb connection to store chat history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from langchain.schema import HumanMessage, AIMessage
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, MongoDBChatMessageHistory
 from prompts import split_prompt_question
 from indexRetriever import doc_retrieval, question_retrieval, question_answer, summarization
 from factchecker import fact_check
@@ -8,12 +8,16 @@ from dotenv import load_dotenv
 if __name__=="__main__":
     load_dotenv()
     
-    chat_history = ConversationBufferMemory()
+    connection_string = "mongodb://localhost:27017"
+    
+    chat_history = MongoDBChatMessageHistory(
+        connection_string=connection_string, session_id="test_session"
+    )
     
     while True:
         print()
         questions = input("Question: ")
-        chat_history.chat_memory.add_user_message(questions)
+        chat_history.add_user_message(questions)
         question_split = split_prompt_question(questions)
         subquestion = question_retrieval(question_split)
         subquestion_answer = ""
@@ -22,8 +26,8 @@ if __name__=="__main__":
             answers = question_answer(subquestion[i], doc)
             checks = fact_check(subquestion[i], answers)
             subquestion_answer += checks["answer"] 
-            # summarize answers
+        # summarize answers
         overall_answer = summarization(subquestion_answer)
         print(overall_answer)
-        chat_history.chat_memory.add_ai_message(overall_answer)
-        # history in mongodb abspeichern
+        chat_history.add_ai_message(overall_answer)
+        print(chat_history.messages)
