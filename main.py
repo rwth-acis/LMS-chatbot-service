5fb37bf8a65c8208cc255195bc66cb0c47966671
Yinnii <y.yin94@outlook.de>
2023-06-30 18:11:34 +0200

    set session_id for each new session

--- main.py
+++ main.py
@@ -1,33 +1,74 @@
-from langchain.schema import HumanMessage, AIMessage
-from langchain.memory import ConversationBufferMemory, MongoDBChatMessageHistory
-from prompts import split_prompt_question
-from indexRetriever import doc_retrieval, question_retrieval, question_answer, summarization
-from factchecker import fact_check
+from langchain.chat_models import ChatOpenAI
+from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
+from langchain.llms import OpenAI
+from langchain.agents.tools import Tool
+from langchain import LLMMathChain, LLMChain
+from langchain.agents import ZeroShotAgent, AgentExecutor, BaseMultiActionAgent
+from langchain.schema import AgentAction, AgentFinish
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_memory import ChatMessageHistory
+from langchain.memory.chat_message_histories import MongoDBChatMessageHistory
+from indexRetriever import answer_retriever
+from prompts import greeting
+import os, uuid
 from dotenv import load_dotenv
+from langchain.callbacks import get_openai_callback
 
-if __name__=="__main__":
-    load_dotenv()
-    
-    connection_string = "mongodb://localhost:27017"
-    
-    chat_history = MongoDBChatMessageHistory(
-        connection_string=connection_string, session_id="test_session"
+load_dotenv()
+# Set up the message history
+def set_mongodb(session_id):
+    connection_string = os.getenv("MONGO_CONNECTION_STRING")
+    message_history = MongoDBChatMessageHistory(
+        connection_string=connection_string, session_id=session_id
     )
-    
+    return message_history
+
+def generate_agent(message_history):
+    llm = OpenAI(temperature=0)
+    tools = [
+        Tool(
+            name="answerQuestion",
+            func=answer_retriever,
+            description="call this to answer the user's question.",
+        ),
+    ]
+
+    prefix = """Have a conversation with a student of the lecture databases and informationssystems, answering the following questions as best you can. Only use the following tools:"""
+    suffix = """Begin! Remember to answer in german.
+
+    {chat_history}
+    Question: {input}
+    {agent_scratchpad}"""
+
+    prompt = ZeroShotAgent.create_prompt(
+        tools,
+        prefix=prefix,
+        suffix=suffix,
+        input_variables=["input", "chat_history", "agent_scratchpad"],
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
+    agent_chain = AgentExecutor.from_agent_and_tools(
+        agent=agent, tools=tools,verbose=True, memory=memory
+    )
+    return agent_chain
+
+if __name__ == "__main__":
+    load_dotenv()
+    message_history = set_mongodb(str(uuid.uuid4()))
     while True:
-        print()
-        questions = input("Question: ")
-        chat_history.add_user_message(questions)
-        question_split = split_prompt_question(questions)
-        subquestion = question_retrieval(question_split)
-        subquestion_answer = ""
-        for i in range(len(subquestion)):
-            doc = doc_retrieval(subquestion[i])
-            answers = question_answer(subquestion[i], doc)
-            checks = fact_check(subquestion[i], answers)
-            subquestion_answer += checks["answer"] 
-        # summarize answers
-        overall_answer = summarization(subquestion_answer)
-        print(overall_answer)
-        chat_history.add_ai_message(overall_answer)
-        print(chat_history.messages)
+        with get_openai_callback() as cb:
+            user_input = input("Du: ")
+            if user_input == "exit":
+                break
+            message_history.add_user_message(user_input)
+            try: 
+                agent = generate_agent(message_history)
+                answer = agent.run(user_input)
+                print(answer)
+                message_history.add_ai_message(answer)
+            except Exception as err:
+                print('Exception occured.', str(err))
+        print(cb)
