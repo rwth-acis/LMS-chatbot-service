7faaa72a76d00866556395f67a03ad0e6ebdb9a8
Yinnii <y.yin94@outlook.de>
2023-07-03 23:40:10 +0200

    outsource agents to separate file

---
+++ agents.py
@@ -0,0 +1,135 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
+from langchain.llms import OpenAI
+from langchain.agents.tools import Tool
+from langchain import LLMMathChain, LLMChain
+from langchain.agents import AgentExecutor, BaseMultiActionAgent, BaseSingleActionAgent, LLMSingleActionAgent, AgentOutputParser
+from langchain.schema import AgentAction, AgentFinish
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_memory import ChatMessageHistory
+from typing import List, Tuple, Any, Union
+from indexRetriever import answer_retriever
+from factchecker import fact_check
+import re
+
+
+class QuestionAgent(BaseSingleActionAgent):
+    """Fake Custom Agent."""
+
+    @property
+    def input_keys(self):
+        return ["input"]
+
+    def plan(
+        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[AgentAction, AgentFinish]:
+        """Given input, decided what to do.
+
+        Args:
+            intermediate_steps: Steps the LLM has taken to date,
+                along with observations
+            **kwargs: User inputs.
+
+        Returns:
+            Action specifying what tool to use.
+        """
+        return AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log="")
+
+    async def aplan(
+        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[AgentAction, AgentFinish]:
+        """Given input, decided what to do.
+
+        Args:
+            intermediate_steps: Steps the LLM has taken to date,
+                along with observations
+            **kwargs: User inputs.
+
+        Returns:
+            Action specifying what tool to use.
+        """
+        return AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log="")
+
+class FakeAgent(BaseMultiActionAgent):
+    """Fake Custom Agent."""
+
+    @property
+    def input_keys(self):
+        return ["input"]
+
+    def plan(
+        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[List[AgentAction], AgentFinish]:
+        """Given input, decided what to do.
+
+        Args:
+            intermediate_steps: Steps the LLM has taken to date,
+                along with observations
+            **kwargs: User inputs.
+
+        Returns:
+            Action specifying what tool to use.
+        """
+        if len(intermediate_steps) == 0:
+            return [
+                AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log=""),
+                AgentAction(tool="factCheck", tool_input=kwargs["input"], log=""),
+            ]
+        else:
+            return AgentFinish(return_values={"output": "bar"}, log="")
+
+    async def aplan(
+        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
+    ) -> Union[List[AgentAction], AgentFinish]:
+        """Given input, decided what to do.
+
+        Args:
+            intermediate_steps: Steps the LLM has taken to date,
+                along with observations
+            **kwargs: User inputs.
+
+        Returns:
+            Action specifying what tool to use.
+        """
+        if len(intermediate_steps) == 0:
+            return [
+                AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log=""),
+                AgentAction(tool="factCheck", tool_input=kwargs["input"], log=""),
+            ]
+        else:
+            return AgentFinish(return_values={"output": "bar"}, log="")
+
+def generate_agent007(message_history):
+    tools = [
+        Tool(
+            name="answerQuestion",
+            func=answer_retriever,
+            description="call this to answer the user's question.",
+            return_direct=True,
+        ),
+    ]
+
+    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
+
+    agent = QuestionAgent()
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+    return agent_executor
+
+def generate_agent008(message_history):
+    tools = [
+        Tool(
+            name="answerQuestion",
+            func=answer_retriever,
+            description="call this to answer the user's question.",
+        ),
+        Tool(
+            name="factCheck",
+            func=fact_check,
+            description="call this to fact check your generated answer",
+        )
+    ]
+    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
+
+    agent = FakeAgent()
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
+    return agent_executor
--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
-from langchain.llms import OpenAI
-from langchain.agents.tools import Tool
-from langchain import LLMMathChain, LLMChain
-from langchain.agents import ZeroShotAgent, AgentExecutor, BaseMultiActionAgent, BaseSingleActionAgent
-from langchain.schema import AgentAction, AgentFinish
-from langchain.memory import ConversationBufferMemory
-from langchain.memory.chat_memory import ChatMessageHistory
 from langchain.memory.chat_message_histories import MongoDBChatMessageHistory
-from indexRetriever import answer_retriever
-from prompts import greeting
-import os, uuid, json, ast
-from typing import List, Tuple, Any, Union
+import os, uuid
 import pymongo
 from dotenv import load_dotenv
 from langchain.callbacks import get_openai_callback
+from agents import generate_agent007, generate_agent008
 
 load_dotenv()
 # Set up the message history
@@ -24,91 +13,6 @@ def set_mongodb(session_id):
         connection_string=connection_string, session_id=session_id
     )
     return message_history
-
-# def generate_agent(message_history):
-#     llm = OpenAI(temperature=0)
-#     tools = [
-#         Tool(
-#             name="answerQuestion",
-#             func=answer_retriever,
-#             description="call this to answer the user's question.",
-#         ),
-#     ]
-
-#     prefix = """You are a tutor for the lecture databases and informationssystems. Have a conversation with a student of this lecture, answering the following questions as best you can. Only use the following tools:"""
-#     suffix = """Begin! Remember to answer in german.
-
-#     {chat_history}
-#     Question: {input}
-#     {agent_scratchpad}"""
-
-#     prompt = ZeroShotAgent.create_prompt(
-#         tools,
-#         prefix=prefix,
-#         suffix=suffix,
-#         input_variables=["input", "chat_history", "agent_scratchpad"],
-#     )
-
-#     memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
-#     llm_chain = LLMChain(llm=llm, prompt=prompt)
-#     agent = ZeroShotAgent(llm_chain=llm_chain, tools=tools, verbose=True)
-#     agent_chain = AgentExecutor.from_agent_and_tools(
-#         agent=agent, tools=tools,verbose=True, memory=memory
-#     )
-#     return agent_chain
-    
-class QuestionAgent(BaseSingleActionAgent):
-    """Fake Custom Agent."""
-
-    @property
-    def input_keys(self):
-        return ["input"]
-
-    def plan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[AgentAction, AgentFinish]:
-        """Given input, decided what to do.
-
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
-
-        Returns:
-            Action specifying what tool to use.
-        """
-        return AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log="")
-
-    async def aplan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[AgentAction, AgentFinish]:
-        """Given input, decided what to do.
-
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
-
-        Returns:
-            Action specifying what tool to use.
-        """
-        return AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log="")
-
-def generate_agent007(message_history):
-    tools = [
-        Tool(
-            name="answerQuestion",
-            func=answer_retriever,
-            description="call this to answer the user's question.",
-            return_direct=True,
-        ),
-    ]
-
-    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
-
-    agent = QuestionAgent()
-    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
-    return agent_executor
 
 if __name__ == "__main__":
     load_dotenv()
