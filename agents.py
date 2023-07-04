398b6edbb4e1a49863b5ba087ee3b6b3a02beb9b
Yinnii <y.yin94@outlook.de>
2023-07-05 00:10:06 +0200

    tests with agents

--- agents.py
+++ agents.py
@@ -4,19 +4,19 @@ from langchain.chat_models import ChatOpenAI
 from langchain.experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
 from langchain.llms import OpenAI
 from langchain.agents.tools import Tool
-from langchain import LLMMathChain, LLMChain
-from langchain.agents import AgentExecutor, BaseMultiActionAgent, BaseSingleActionAgent, LLMSingleActionAgent, AgentOutputParser
+from langchain import LLMMathChain, LLMChain, PromptTemplate
+from langchain.agents import AgentType, AgentExecutor, ZeroShotAgent, ConversationalChatAgent, BaseMultiActionAgent, BaseSingleActionAgent, LLMSingleActionAgent, AgentOutputParser, initialize_agent
 from langchain.schema import AgentAction, AgentFinish
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, ReadOnlySharedMemory, ConversationBufferWindowMemory
 from langchain.memory.chat_memory import ChatMessageHistory
 from typing import List, Tuple, Any, Union
 from indexRetriever import answer_retriever
+from questionGenerator import random_question_tool
 from factchecker import fact_check
 import re
-
+from dotenv import load_dotenv
 
 class QuestionAgent(BaseSingleActionAgent):
-    """Fake Custom Agent."""
 
     @property
     def input_keys(self):
@@ -51,55 +51,6 @@ class QuestionAgent(BaseSingleActionAgent):
             Action specifying what tool to use.
         """
         return AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log="")
-
-class FakeAgent(BaseMultiActionAgent):
-    """Fake Custom Agent."""
-
-    @property
-    def input_keys(self):
-        return ["input"]
-
-    def plan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[List[AgentAction], AgentFinish]:
-        """Given input, decided what to do.
-
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
-
-        Returns:
-            Action specifying what tool to use.
-        """
-        if len(intermediate_steps) == 0:
-            return [
-                AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log=""),
-                AgentAction(tool="factCheck", tool_input=kwargs["input"], log=""),
-            ]
-        else:
-            return AgentFinish(return_values={"output": "bar"}, log="")
-
-    async def aplan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[List[AgentAction], AgentFinish]:
-        """Given input, decided what to do.
-
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
-
-        Returns:
-            Action specifying what tool to use.
-        """
-        if len(intermediate_steps) == 0:
-            return [
-                AgentAction(tool="answerQuestion", tool_input=kwargs["input"], log=""),
-                AgentAction(tool="factCheck", tool_input=kwargs["input"], log=""),
-            ]
-        else:
-            return AgentFinish(return_values={"output": "bar"}, log="")
 
 def generate_agent007(message_history):
     tools = [
@@ -109,6 +60,12 @@ def generate_agent007(message_history):
             description="call this to answer the user's question.",
             return_direct=True,
         ),
+        Tool(
+            name="tutorQuestion",
+            func=random_question_tool,
+            description="call this to generate a question for the user to answer.",
+            return_direct=True,
+        )
     ]
 
     memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
@@ -117,21 +74,31 @@ def generate_agent007(message_history):
     agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
     return agent_executor
 
-def generate_agent008(message_history):
+def generate_agent009():
     tools = [
         Tool(
             name="answerQuestion",
             func=answer_retriever,
             description="call this to answer the user's question.",
+            return_direct=True,
         ),
         Tool(
-            name="factCheck",
-            func=fact_check,
-            description="call this to fact check your generated answer",
+            name="tutorQuestion",
+            func=random_question_tool,
+            description="call this to generate a question for the user to answer if the user wants to study for the lecture.",
+            return_direct=True,
         )
     ]
-    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=message_history)
 
-    agent = FakeAgent()
-    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True, memory=memory)
-    return agent_executor
+    llm = ChatOpenAI(temperature=0)
+    # memory = ConversationBufferWindowMemory(k=10)
+    agent_chain = initialize_agent(
+        tools, 
+        llm, 
+        agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, 
+        verbose=True,
+        # memory=memory,
+    )
+    
+    return agent_chain
+
